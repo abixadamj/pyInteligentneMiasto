767ac554235909b5b7694053f0a6f96b54a5f23a
wrata <wrata@poczta.onet.pl>
2017-02-04 18:06:12 +0100

    Update test_lamp.py

--- wrata_functions/test_lamp.py
+++ wrata_functions/test_lamp.py
@@ -10,30 +10,30 @@ from time import sleep
 # Lista dostępnych urządzeń
 city.info()
 
-print('Włączenie latarni ulicznej z pełną mocą')
+print(u'Włączenie latarni ulicznej z pełną mocą')
 # Włączenie latarni ulicznej - pełna moc świecenia
 lamp.on()
 sleep(3)
 
-print('Włączenie latarni ulicznej z połową mocy')
+print(u'Włączenie latarni ulicznej z połową mocy')
 # Włączenie latarni ulicznej - tryb ekonomiczny
 lamp.on_eco()
 sleep(3)
 
-print('Wyłączenie latarni ulicznej')
+print(u'Wyłączenie latarni ulicznej')
 # Wyłączenie latarni ulicznej
 lamp.off()
 sleep(3)
 
 try:
     # Włączanie/wyłączenie latarni ulicznej w zależności od natężenia światła w otoczeniu
-    print('Automatyczne sterowanie latarnią uliczną - pomiar natężenia światła')
+    print(u'Automatyczne sterowanie latarnią uliczną - pomiar natężenia światła')
     while True:
         if lamp.is_dark():
-            print('jest ciemno')
+            print(u'jest ciemno')
             lamp.on()
         else:
-            print('jest jasno')
+            print(u'jest jasno')
             lamp.off()
         sleep(0.5) # pomiar natężenia co pół sekundy
         
